5cb7118bbf5afca223d7b53939ff54e8ad7099c2
Justus Christinck <justus.christinck@gmx.de>
2024-12-01 23:56:35 +0100

    removed redundant file reading, now 3 lines instead of 2...

--- day01.py
+++ day01.py
@@ -1,2 +1,3 @@
-print(sum([abs(z[1] - z[0]) for z in zip(*[sorted(y) for y in zip(*[[int(x) for x in line.split()] for line in open("input/day01.txt", "r").read().splitlines()])])]))  # 1941353
-print(sum([z * [y for y in zip(*[[int(x) for x in line.split()] for line in open("input/day01.txt", "r").read().splitlines()])][1].count(z) for z in [y for y in zip(*[[int(x) for x in line.split()] for line in open("input/day01.txt", "r").read().splitlines()])][0]]))  # 22539317
+inp = [[int(x) for x in line.split()] for line in open("input/day01.txt", "r").read().splitlines()]
+print(sum([abs(z[1] - z[0]) for z in zip(*[sorted(y) for y in zip(*inp)])]))  # 1941353
+print(sum([z * [y for y in zip(*inp)][1].count(z) for z in [y for y in zip(*inp)][0]]))  # 22539317
